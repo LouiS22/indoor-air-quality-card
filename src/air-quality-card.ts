05ce14c557d131c550fcfaa73806b07c5b66a0f7
Attila Lukács <lukacs.attila1981@gmail.com>
2026-05-14 14:15:21 +0200

    Add files via upload

--- src/air-quality-card.ts
+++ src/air-quality-card.ts
@@ -17,18 +17,18 @@ console.info(
 
 (window as any).customCards = (window as any).customCards || [];
 (window as any).customCards.push({
-  type: 'air-quality-card',
-  name: 'Air Quality Card',
+  type: 'indoor-air-quality-card',
+  name: 'Indoor Air Quality Card',
   description: 'Monitor indoor air quality (CO2, PM2.5, VOC, humidity, temperature, etc.)',
   preview: true,
-  documentationURL: 'https://github.com/wilsto/air-quality-card',
+  documentationURL: 'https://github.com/LouiS22/indoor-air-quality-card',
 });
 
 @customElement('air-quality-card')
 export class AirQualityCard extends MonitorCardBase {
   static CARD_INFO: CardInfo = {
-    cardType: 'air-quality-card',
-    cardName: 'Air Quality Card',
+    cardType: 'indooar-air-quality-card',
+    cardName: 'Indoor Air Quality Card',
     cardDescription:
       'A Home Assistant card for monitoring indoor and outdoor air quality (CO2, PM2.5, VOC, humidity, etc.)',
   };
@@ -36,7 +36,7 @@ export class AirQualityCard extends MonitorCardBase {
   static SENSORS: SensorsRegistry = AIR_QUALITY_SENSORS;
 
   static IMAGE_BASE_URL =
-    'https://raw.githubusercontent.com/wilsto/air-quality-card/master/resources';
+    'https://raw.githubusercontent.com/LouiS22/indoor-air-quality-card/master/resources';
 
   static async getConfigElement(): Promise<HTMLElement> {
     await import('./editor.js');
